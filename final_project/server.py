f51298fb78721eacf49ff63ed08c4012e17cbab2
Alex-Franky Djieufo Ngoufack <96366217+Alexfranky@users.noreply.github.com>
2023-01-04 18:38:05 +0100

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,23 +1,23 @@
-from machinetranslation import translator
+import machinetranslation
 from flask import Flask, render_template, request
 import json
-import machinetranslation
+from machinetranslation import translator
 
 app = Flask("Web Translator")
 
-@app.route("/english_to_french")
+@app.route("/englishToFrench")
 def english_to_french():
-    text_to_translate_f = request.args.get('Hello')
-    french_translation= language_translator.translate(text= text_to_translate_f, model_id="en-fr").get_result()
-    return french_translation.get['translations'][0].get['translation']
+    textToTranslate = request.args.get('textToTranslate')
+    french_text= translator.english_to_french(textToTranslate)
+    return french_text
 
-@app.route("/french_to_english")
+@app.route("/frenchToEnglish")
 def french_to_english():
-    text_to_translate_e = request.args.get('Bonjour')
-    english_translation= language_translator.translate(text= text_to_translate_e, model_id="fr-en").get_result()
-    return english_translation.get['translations'][0].get['translation']
+    textToTranslate = request.args.get('textToTranslate')
+    english_text = translator.french_to_english(textToTranslate)
+    return english_text
 
-@app.route("/index.html")
+@app.route("/")
 def renderIndexPage():
     return render_template('index.html')
 if __name__ == "__main__":
